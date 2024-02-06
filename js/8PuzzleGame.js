362a718be60f3bbf29991c4f3ff89d82644d6f64
yechezkel chen <yl9753@gmail.com>
2024-02-06 23:51:35 +0200

    Begin the 8 puzzle game

---
+++ js/8PuzzleGame.js
@@ -0,0 +1,24 @@
+const dragstart_handler = ev => {
+    console.log("dragstart")
+    ev.dataTransfer.setData("text/plain", ev.target.id)
+    ev.dataTransfer.dropEffect = "move";
+}
+
+const dragover_handler = ev => {
+    console.log("dragOver");
+    ev.preventDefault();
+}
+
+const drop_handler = ev => {
+    console.log("drag")
+    ev.preventDefault();
+    // Get the id of the target and add the moved element to the target's DOM
+    const data = ev.dataTransfer.getData("text/plain");
+    ev.target.innerText = document.getElementById(data).innerText;
+}
+
+const dragend_handler = ev => {
+    console.log("dragEnd");
+    // Remove all of the drag data
+    ev.dataTransfer.clearData();
+}
