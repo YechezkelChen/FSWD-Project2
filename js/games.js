4a5eee2b782a7fe30a961c319e80e91fb5357603
meirisrael <meirisrael770@gmail.com>
2024-02-07 20:30:59 +0200

    mainContent + game 2

--- js/games.js
+++ js/games.js
@@ -4,4 +4,20 @@ function startGame1() {
 
 function startGame2() {
     alert("Game 2 started!");
+    var flappyFrame = document.getElementById("flappy");
+    var overlay = document.createElement("div");
+    overlay.classList.add("overlay");
+    document.body.appendChild(overlay);
+
+    flappyFrame.style.display = "block";
+    flappyFrame.src = "../html/flappyBall.html";
+
+    overlay.addEventListener("click", function() {
+        flappyFrame.classList.remove("show");
+        overlay.style.display = "none";
+    });
+
+    flappyFrame.classList.add("show");
+    flappyFrame.src = "../html/flappyBall.html"; // Change this to the actual URL of Game 2
+    overlay.style.display = "block";
 }
