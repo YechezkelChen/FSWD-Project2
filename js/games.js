637388a1358f908e89949b95099b023276beb3a6
meirisrael <meirisrael770@gmail.com>
2024-02-13 17:53:59 +0200

    fix_bug

--- js/games.js
+++ js/games.js
@@ -9,3 +9,17 @@ function startFlapyBallGame() {
 function in_progress() {
     window.open('../html/inProgress.html', replace = '_self');
 }
+
+function openTab(evt, tabName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablink");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
